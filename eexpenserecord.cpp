a57053c9f03d13f091b894b6335ea33ecc172b48
MrEnotEnotovich <kirill123456789094@gmail.com>
2022-12-27 23:49:16 +0300

    Added realization of InputExpense in eexpenserecord.cpp

--- eexpenserecord.cpp
+++ eexpenserecord.cpp
@@ -18,3 +18,41 @@ ExpenseRecord::ExpenseRecord(int Year,float Jan,float Feb,float Mar,float Apr,fl
     this->Nov = Nov;// расходы за Ноябрь
     this->Dec = Dec;// расходы за Декабрь
 }
+
+void ExpenseRecord::InputExpense()//ввод затрат за год  по месяцам
+{
+    int *YearIn = new int;
+    int *JanIn = new int;
+    int *FebIn = new int;
+    int *MarIn = new int;
+    int *AprIn = new int;
+    int *MayIn = new int;
+    int *JunIn = new int;
+    int *JulIn = new int;
+    int *AugIn = new int;
+    int *SepIn = new int;
+    int *OctIn = new int;
+    int *NovIn = new int;
+    int *DecIn = new int;
+
+
+    cout<<"Enter Year"<<endl;
+    cin>>*YearIn;
+
+    cout<<"Enter all Expenses from January with spaces in between\n";
+    cin>>*JanIn>>*FebIn>>*MarIn>>*AprIn>>*MayIn>>*JunIn>>*JulIn>>*AugIn>>*SepIn>>*OctIn>>*NovIn>>*DecIn;
+    v.push_back(ExpenseRecord(*YearIn,*JanIn,*FebIn,*MarIn,*AprIn,*MayIn,*JunIn,*JulIn,*AugIn,*SepIn,*OctIn,*NovIn,*DecIn));
+    delete YearIn;
+    delete JanIn;
+    delete FebIn;
+    delete MarIn;
+    delete AprIn;
+    delete MayIn;
+    delete JunIn;
+    delete JulIn;
+    delete AugIn;
+    delete SepIn;
+    delete OctIn;
+    delete NovIn;
+    delete DecIn;
+}
